0dc966291b80f6a5096940f488d2e94d9174b5fe
ochibi1 <lapotit0122@gmail.com>
2020-07-16 18:21:48 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,10 @@ module GamePlan
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.time_zone = 'Asia/Tokyo'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
+    # config.i18n.available_locales = [:ja, :en]
+    config.time_zone = 'Asia/Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
