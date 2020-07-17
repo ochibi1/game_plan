a86652f9518df613967614568cab182ae2daa32f
ochibi1 <lapotit0122@gmail.com>
2020-07-18 00:16:59 +0900

    refile secret key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f9650ab7a6fba4cf83c0bef17e17397db6ae955b0096ff18ba3eac1bf3ece768f7738f0696a7b056a9e9c0f978642064bc1932f612e5e6aeb038db2211e4506c'
