a42af57ac73123801714ffcce7b234101c894637
ochibi1 <lapotit0122@gmail.com>
2020-07-21 18:16:29 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users, controllers: {
         registrations: 'users/registrations',
         sessions: 'users/sessions'
@@ -7,5 +8,7 @@ Rails.application.routes.draw do
   root "articles#index"
   resources :articles do
     resources :comments, only: :create
+    resource :favorites, only: [:create, :destroy]
   end
+  resources :users, only: :show
 end
