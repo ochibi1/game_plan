e7c41abcaddd2e28f93ec9796360f73a52ab747a
ochibi1 <lapotit0122@gmail.com>
2020-08-14 13:18:24 +0900

    update for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.14.1"
 
 set :application, "game_plan"
-set :repo_url, "git@example.com:ochibi1/game_plan.git"
+set :repo_url, "git@github.com:ochibi1/game_plan.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
